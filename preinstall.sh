0a79988f1a300d5897ef9bbf064273b28db57ba9
Jeffrey He <100673826+jeffreyheping@users.noreply.github.com>
2024-10-24 15:22:35 +0800

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -10,3 +10,7 @@ sudo snap install snap-store
 
 
 
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub io.github.prateekmedia.appimagepool
+
