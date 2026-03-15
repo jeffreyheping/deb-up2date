4f5011d753425c751e94738604520b6a8e99bf82
Jeffrey He <100673826+jeffreyheping@users.noreply.github.com>
2026-03-15 16:00:08 +0800

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,8 +1,3 @@
-sudo apt install curl ca-certificates -y
-curl -s https://repo.waydro.id | sudo bash
-sudo apt install waydroid -y
-
-
 
 sudo apt install snapd
 sudo snap install snapd
@@ -14,3 +9,4 @@ sudo apt install flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub io.github.prateekmedia.appimagepool
 
+sudo apt install byobu
